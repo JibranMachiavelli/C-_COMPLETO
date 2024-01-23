4f2d81c3111930686f41adc81dfcb595c06745dd
Leonardo jibran Machiavelli <jibranind@gmail.com>
2024-01-22 22:42:52 -0300

    IniciandoFirstCommit

---
+++ 03LogicaProgramaçãoUsandoC#/01Recapitulando/01Recapitulando/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            int idade = 32;
+            string nome = "Maria";
+            double saldo = 120.22;
+
+            Console.WriteLine(nome  + " tem, " + idade + " anos  e tem saldo igual a " + saldo + " reais.");
+        }
+    }
+}
