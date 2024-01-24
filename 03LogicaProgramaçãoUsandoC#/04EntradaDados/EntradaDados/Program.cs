1c264ad69115c17fc111ba3ea1380a55a484a016
Leonardo jibran Machiavelli <jibranind@gmail.com>
2024-01-24 00:00:52 -0300

    Part01 entrada de dados e uso de split, Part02 uso de numeros na leitura soma e especiais, Exercicio Pratico utilizando split e resultados

--- 03LogicaProgramaçãoUsandoC#/04EntradaDados/EntradaDados/Program.cs
+++ 03LogicaProgramaçãoUsandoC#/04EntradaDados/EntradaDados/Program.cs
@@ -1,20 +1,106 @@
 ﻿using System;
+using System.Globalization;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program {
         static void Main(string[] args) {
 
+            //PART 01
+            /*
             string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+
+            //string s = Console.ReadLine();
+
+            string[] v = Console.ReadLine().Split(' ');
+            string a = v[0];
+            string b = v[1];
+            string c = v[2];
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Voce digitou : ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            Console.WriteLine("");
             
-            Console.WriteLine("Voce digitou : " + frase);
+            //PART 02
 
-            int y = int.Parse(Console.ReadLine());
-            int x = int.Parse(Console.ReadLine());
-            int z = x + y;
+            Console.WriteLine("Digite mais 2 valores inteiros: ");
+            int o = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            int i = o + p;
 
-            Console.WriteLine("A soma é : " + z);
+            Console.WriteLine("A soma é : " + i);
 
+            Console.WriteLine("");
+
+            Console.WriteLine("Digite um numero int um caractere char e um numero double: ");
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("");
+            
+            Console.WriteLine("Digite o nome sexo idade altura um ao lado do outro: ");
+            string[] vet = Console.ReadLine().Split(' ');
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Voce digitou: ");
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+            */
+            //Exercicio
+
+            Console.WriteLine("Entre com  seu nome completo: ");
+            string nome  = Console.ReadLine();
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Quantos quartos tem na sua casa? ");
+            int quartos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Entre com o preco de um produto: ");
+            double preco1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Entre com seu último nome, idade e altura(mesma linha): ");
+            string[] v = Console.ReadLine().Split(' ');
+            string ultimoNome = v[0];
+            int idade = int.Parse(v[1]);
+            double altura = double.Parse(v[2]);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Resultado: ");
+            Console.WriteLine(nome);
+            Console.WriteLine(quartos);
+            Console.WriteLine(preco1.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(ultimoNome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
